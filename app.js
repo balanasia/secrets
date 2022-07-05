cc64915432035acf59eca5c00bb09beacd0f8818
balanasia <anabalasia@gmail.com>
2022-07-05 17:50:33 -0500

    Add md5 hash to the passwords when registering and comparing the stored password with a hash stored in the database

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -29,7 +29,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 //create mongoose model to use the user schema
 const User = new mongoose.model("User", userSchema);
@@ -52,7 +51,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if(!err){
@@ -67,7 +66,7 @@ app.post("/register", function(req, res){
 //check if the password that the user put in is the same as in the database
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   //search our mongoDB database for the email
   User.findOne({email: username}, function(err, foundUser){
